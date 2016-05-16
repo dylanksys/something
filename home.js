f5e8aa7b2b82d54d534d8d52b7d9341179bf6a7a
dylanksys <j:null>
2016-05-16 10:36:10 -0700

    Initial Commit

---
+++ home.js
@@ -0,0 +1,13 @@
+var router = require('express').Router();
+
+// Change API methods and endpoint methods to fit your needs
+
+router.get('/home', function(req, res) {
+	res.send('Path: /home');
+};
+
+router.get('/about', function(req, res) {
+	res.send('Path: /about');
+};
+
+module.exports = router;
